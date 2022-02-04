3d242b400be78df99a79b24033e31b7e0f277a78
Gloria Cheung <gloriacheung812@gmail.com>
2022-02-04 19:07:44 +0000

    reformat examples

--- conditionals.js
+++ conditionals.js
@@ -1,34 +1,34 @@
-// const raining = true;
-// const cold = false;
+const raining = true;
+const cold = false;
 
-// if (raining) {
-//   console.log("Don't forget your umbrella!");
-// }
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
 
-// if (cold) {
-//   console.log("Make sure you pick out a scarf!");
-// }
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
 
-// console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");
 
-// const temperature = 12;
+const temperature = 12;
 
-// if (temperature < 0) {
-//   console.log("Make sure you pick out a scarf!");
-// } else if (temperature < 15) {
-//   console.log("Short sleeves won't cut it!");
-// } else {
-//   console.log("Short sleeves are fine.");
-// }
+if (temperature < 0) {
+  console.log("Make sure you pick out a scarf!");
+} else if (temperature < 15) {
+  console.log("Short sleeves won't cut it!");
+} else {
+  console.log("Short sleeves are fine.");
+}
 
-// console.log("Now you're ready to go outside!");
+console.log("Now you're ready to go outside!");
 
-const temperature = 45;
+
 if (temperature < -40 || temperature > 40) {
   console.log("Maybe going outside isn't such a great idea…");
 }
 
-const raining = false; 
+
 if (!raining) {
   console.log("Leave your umbrella at home!");
 }
