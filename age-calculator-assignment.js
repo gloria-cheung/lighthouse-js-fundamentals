2e0194752a81b41aa288dabc9a4a374a246476ec
Gloria Cheung <gloriacheung812@gmail.com>
2022-02-06 16:18:28 +0000

    solution to assignment

---
+++ age-calculator-assignment.js
@@ -0,0 +1,7 @@
+const ageCalculator = function (name, yearOfBirth, currentYear) {
+  const age = currentYear - yearOfBirth;
+  return `${name} is ${age} years old.`
+}
+
+console.log(ageCalculator("Miranda", 1983, 2015));
+// -> Miranda is 32 years old.
